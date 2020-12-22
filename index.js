7d98ed83e9ae664525133e876f9c50c193a082e1
LPF33 <larspefe@gmail.com>
2020-12-22 22:32:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.use("/memory", express.static(__dirname + "/games/Memory"));
+app.use("/rabbittracker", express.static(__dirname + "/games/RabbitTracker"));
+app.use("/spaceinvaders", express.static(__dirname + "/games/SpaceInvaders"));
+app.use("/sudoku", express.static(__dirname + "/games/Sudoku"));
+
+app.get("/memory", (req, res) => {
+    res.sendFile(__dirname + "/views/memory.html");
+});
+
+app.get("/rabbittracker", (req, res) => {
+    res.sendFile(__dirname + "/views/rabbit.html");
+});
+
+app.get("/spaceinvaders", (req, res) => {
+    res.sendFile(__dirname + "/views/invaders.html");
+});
+
+app.get("/sudoku", (req, res) => {
+    res.sendFile(__dirname + "/views/sudoku.html");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/*", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 8080, () => console.log("Server running"));
