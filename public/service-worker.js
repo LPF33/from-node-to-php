3a7093b44f03ef49b0507faafcd44349ff3433a8
LPF33 <larspefe@gmail.com>
2021-07-11 10:15:49 +0200

    update again sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,17 @@
 self.addEventListener("install", function (event) {
     event.waitUntil(
-        caches.open("lpf-games").then(function (cache) {
+        caches.open("lpf-games1").then(function (cache) {
             return cache.addAll([
                 "/",
                 "/index.html",
                 "/style.css",
                 "/elephant.js",
                 "/games.json",
-                "/Braintrainer.PNG",
-                "/Memory.PNG",
-                "/RabbitTracker.PNG",
-                "/SpaceInvaders.PNG",
-                "/sudoku.PNG",
+                "/images/Braintrainer.PNG",
+                "/images/Memory.PNG",
+                "/images/RabbitTracker.PNG",
+                "/images/SpaceInvaders.PNG",
+                "/images/sudoku.PNG",
             ]);
         })
     );
@@ -21,6 +21,22 @@ self.addEventListener("fetch", function (event) {
     event.respondWith(
         caches.match(event.request).catch(function () {
             return fetch(event.request);
+        })
+    );
+});
+
+this.addEventListener("activate", function (event) {
+    var cacheWhitelist = ["lpf-games1"];
+
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(
+                keyList.map(function (key) {
+                    if (cacheWhitelist.indexOf(key) === -1) {
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
     );
 });
