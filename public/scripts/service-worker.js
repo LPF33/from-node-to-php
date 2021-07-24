b052991ab149dd38510306152fe126a9e897e3aa
LPF33 <larspefe@gmail.com>
2021-07-24 11:02:20 +0200

    porjects added, sw-vs-11

--- public/scripts/service-worker.js
+++ public/scripts/service-worker.js
@@ -12,7 +12,7 @@ const filesToCache = [
     "/no-connection.html",
 ];
 
-const cacheName = "lpf-games9";
+const cacheName = "sw-lpf-games-v11";
 
 self.addEventListener("install", function (event) {
     event.waitUntil(
