cc4224a3fdd7a1423dc0db96d0be17870b18313f
LPF33 <larspefe@gmail.com>
2021-09-30 22:31:37 +0200

    update 30.9.2021

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,7 @@ const filesToCache = [
     "/no-connection.html",
 ];
 
-const cacheName = "sw-lpf-games-v19";
+const cacheName = "sw-lpf-games-v20";
 
 self.addEventListener("install", function (event) {
     event.waitUntil(
