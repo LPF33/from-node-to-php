2fed2f725f32030fe155d24aad8e46172178c04a
lfeigl <larspefe@gmail.com>
2022-07-16 23:33:38 +0200

    build(bepsvpt/secure-headers): Added security related headers to HTTP response

--- public/index.php
+++ public/index.php
@@ -4,10 +4,15 @@ $serverDir = __DIR__.'/../';
 require $serverDir . '/vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use \Bepsvpt\SecureHeaders\SecureHeaders;
 
 $logger = new Logger('website');
 $logger->pushHandler(new StreamHandler("$serverDir.app.log", Logger::INFO));
 $logger->info('Site called: '. $_SERVER["REQUEST_URI"]);
+
+$secureHeaders = SecureHeaders::fromFile($serverDir.'src/secure-headers.php');
+$secureHeaders->headers();
+$secureHeaders->send();
 
 require $serverDir.'src/bootstrap.php';
 
