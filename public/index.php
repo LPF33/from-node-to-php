615af64f6454095eb45c71ba1806c18b89350590
lfeigl <larspefe@gmail.com>
2022-08-05 12:19:33 +0200

    refactor(index.php): production logging stream

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 $serverDir = __DIR__.'/../';
+define('PRODUCTION', !str_starts_with($_SERVER['HTTP_HOST'],'localhost'));
 
 require $serverDir . '/vendor/autoload.php';
 use Monolog\Logger;
@@ -7,7 +8,7 @@ use Monolog\Handler\StreamHandler;
 use \Bepsvpt\SecureHeaders\SecureHeaders;
 
 $logger = new Logger('website');
-$logger->pushHandler(new StreamHandler($serverDir.'app.log', Logger::INFO));
+$logger->pushHandler(new StreamHandler(PRODUCTION ? 'php://stderr' : $serverDir.'app.log', Logger::INFO));
 $logger->info('Site called: '. $_SERVER["REQUEST_URI"].' from: '.$_SERVER["REMOTE_ADDR"]);
 
 $secureHeaders = SecureHeaders::fromFile($serverDir.'src/secure-headers.php');
